3c2a92bb4e5fcba0b505f81f535b9dad2e71c940
unmoha <anwarmoha7926@gmail.com>
2025-04-07 10:48:17 -0700

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -5,28 +5,27 @@ double add(double a, double b) {
 }
 
 int main() {int num1, num2;
-    char operation;
-    cout << "Enter two numbers: ";
-    cin >> num1 >> num2;
-    cout << "Choose operation (+, -, *, /): ";
-    cin >> operation;
-    switch (operation) {
-    case '+':
-   
-    break;
-    case '-':
-   
-    break;
-    case '*':
-  
-    break;
-    case '/':
-
-    break;
-    default:
-    cout << "Invalid operation!" << endl;
-    }
-    return 0;
+ char operation;
+ cout << "Enter two numbers: ";
+ cin >> num1 >> num2;
+ cout << "Choose operation (+, -, *, /): ";
+ cin >> operation;
+ switch (operation) {
+ case '+':
+ cout << "Result: " << add(num1, num2) << endl;
+ break;
+ case '-':
+ cout << "Result: " << subtract(num1, num2) << endl;
+ break;
+ case '*':
+ cout << "Result: " << multiply(num1, num2) << endl;
+ break;
+ case '/':
+ cout << "Result: " << divide(num1, num2) << endl;
+ break;
+ default:
+ cout << "Invalid operation!" << endl;
+ }
    }
 
 
